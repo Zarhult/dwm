5745f3b0e302e2ab36d3696924dcbb026af7999e
Calvin Glisson <cglis001@ucr.edu>
2020-06-05 20:48:37 -0700

    Xrdb patch, new keybinds

--- config.def.h
+++ config.def.h
@@ -2,20 +2,21 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
@@ -34,7 +35,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -44,7 +45,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -56,34 +57,62 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[]   =   { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *termcmd[]        =   { "st", NULL };
+static const char *voldwncmd[]      =   { "pamixer", "--decrease", "5", NULL                };
+
+/* my commands */
+static const char *volupcmd[]       =   { "pamixer", "--increase", "5", NULL                    };
+static const char *volmutecmd[]     =   { "pamixer", "--toggle-mute", NULL                      };
+static const char *brtdwncmd[]      =   { "xbacklight", "-10", "-time", "0", NULL               };
+static const char *brtupcmd[]       =   { "xbacklight", "+10", "-time", "0", NULL               };
+static const char *nnncmd[]         =   { "st", "-e", "bash", "-i", "-c", "n", NULL             };
+static const char *ncmpcppcmd[]     =   { "st", "-e", "bash", "-i", "-c", "ncmpcpp", NULL       };
+static const char *papecmd[]        =   { "wpg", "-m", NULL                                     };
+static const char *wpgcmd[]         =   { "wpg", NULL                                           };
+static const char *ffcmd[]          =   { "firefox", NULL                                       };
+static const char *emacscmd[]       =   { "emacs", NULL                                         };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	/* modifier                     key                 function        argument */
+	{ MODKEY,                       XK_p,               spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return,          spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,               togglebar,      {0} },
+	{ MODKEY,                       XK_j,               focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,               focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,               incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,               incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,               setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,               setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_Return,          zoom,           {0} },
+	{ MODKEY,                       XK_Tab,             view,           {0} },
+	{ MODKEY,                       XK_q,               killclient,     {0} },
+	{ MODKEY,                       XK_t,               setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,               setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,               setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,           setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,           togglefloating, {0} },
+	{ MODKEY,                       XK_0,               view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,               tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,           focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,          focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,           tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,          tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_F5,              xrdb,           {.v = NULL } },
+
+    /* my keybinds */
+    { MODKEY,                       XK_bracketleft,     spawn,          {.v = voldwncmd } }, 
+    { MODKEY,                       XK_bracketright,    spawn,          {.v = volupcmd } }, 
+    { MODKEY,                       XK_backslash,       spawn,          {.v = volmutecmd } }, 
+    { MODKEY|ShiftMask,             XK_bracketleft,     spawn,          {.v = brtdwncmd } }, 
+    { MODKEY|ShiftMask,             XK_bracketright,    spawn,          {.v = brtupcmd } }, 
+    { MODKEY,                       XK_n,               spawn,          {.v = nnncmd } }, 
+    { MODKEY,                       XK_r,               spawn,          {.v = ncmpcppcmd } }, 
+    { MODKEY,                       XK_slash,           spawn,          {.v = papecmd } }, 
+    { MODKEY,                       XK_u,               spawn,          {.v = wpgcmd } }, 
+    { MODKEY,                       XK_o,               spawn,          {.v = ffcmd } }, 
+    { MODKEY,                       XK_e,               spawn,          {.v = emacscmd } }, 
+
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
